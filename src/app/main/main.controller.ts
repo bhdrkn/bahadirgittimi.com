a29eb5f17ce9d38d4403f4467dba51fba34f0709
Bahadır AKIN <bhdrkn@gmail.com>
2015-10-11 16:25:02 +0300

    Dialog is added

--- src/app/main/main.controller.ts
+++ src/app/main/main.controller.ts
@@ -6,13 +6,27 @@ module bahadirgittimi {
     public imageURL : string;
     public answerText : string;
 
+    private dialogService : ng.material.IDialogService;
+
     /* @ngInject */
-    constructor (yesNoService : YesNoService, expectedAnswer : string) {
+    constructor (yesNoService : YesNoService, expectedAnswer : string, $mdDialog : ng.material.IDialogService) {
       this.awesomeThings = new Array();
       yesNoService.getGif(expectedAnswer).then((data : any) =>{
         this.imageURL = data.image;
       });
       this.answerText = ('no' === expectedAnswer ? 'HAYIR' : 'EVET');
+      this.dialogService = $mdDialog;
+    }
+
+    public showAlert(event : MouseEvent){
+      this.dialogService.show(
+        this.dialogService.alert().title('Yakında...')
+                                  .content('Tüm tarihler burada özetlenece...')
+                                  .ariaLabel('Summary')
+                                  .ok('Hımm...')
+                                  .targetEvent(event)
+                                  .clickOutsideToClose(true)
+      );
     }
   }
 }
